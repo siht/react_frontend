71c800eceeba3f80c576ab02975175297385b2e4
siht <carlos.tz.00.00@gmail.com>
2020-05-10 22:48:26 -0500

    refactor to use with API calls

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,20 +1,28 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import Carousel from '@brainhubeu/react-carousel';
 import '@brainhubeu/react-carousel/lib/style.css';
 
-function ImageList(props) {  
+function ImageList(props) {
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const payload = await fetch(process.env.REACT_APP_API_URL+"profiles");
+            const jsonData = await payload.json();
+            setData(jsonData);
+        };
+        fetchData();
+    }, []);
+
     return (
         <div className="image-list">
             <Carousel arrows dots>
-                <img src="https://cuarzos.info/wp-content/uploads/2019/10/piedras-verdes.jpg" />
-                <img src="https://www.gemselect.com/graphics/green-gemstone-list-compilation-gemselect.jpg" />
-                <img src="https://significadodelaspiedras.com/wp-content/uploads/2017/09/miedras-verdes.jpg" />
-                <img src="https://www.gemselect.com/graphics/gems-by-color-green-gems-gemselect.jpg" />
-                <img src="https://comps.canstockphoto.es/piedras-conjunto-verde-piedras-retrato_csp36721419.jpg" />
-                <img src="https://significadodelaspiedras.club/wp-content/uploads/2019/10/significado-de-las-piedras-verdes.jpg" />
+                {data.map(item => (
+                    <img src={item.imagen} alt={item.titulo} />
+                ))}
             </Carousel>
         </div>
-    )
+    );
 }
 
 export default ImageList
